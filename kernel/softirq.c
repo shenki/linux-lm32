81882876e003a579089f15e43d55b1c85bc8fede
Takeshi MATSUYA <macchan@sfc.wide.ad.jp>
2010-02-04 01:51:49 +0900

    adhoc patch (for kernel debug)

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -147,7 +147,8 @@ EXPORT_SYMBOL(_local_bh_enable);
 
 void __local_bh_enable_ip(unsigned long ip, unsigned int cnt)
 {
-	WARN_ON_ONCE(in_irq() || irqs_disabled());
+//	WARN_ON_ONCE(in_irq() || irqs_disabled());
+	WARN_ON_ONCE(in_irq());
 #ifdef CONFIG_TRACE_IRQFLAGS
 	local_irq_disable();
 #endif
