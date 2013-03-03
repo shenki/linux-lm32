0bfdb9b79bc73d8ab39747dde0faf24001ab3426
Lars-Peter Clausen <lars@metafoo.de>
2013-03-03 11:59:50 +0100

    lm32: timex.h: Remove CLOCK_TICK_RATE
    It's unused.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

--- arch/lm32/include/asm/timex.h
+++ arch/lm32/include/asm/timex.h
@@ -24,14 +24,6 @@
 #ifndef _ASM_TIMEX_H
 #define _ASM_TIMEX_H
 
-#ifdef __KERNEL__
-
-/* cannot use value supplied by bootloader because this value is used in an #if */
-/* 100 MHz works as default value, even for 75 MHz bitstreams */
-#define CLOCK_TICK_RATE		(100*1000*1000)
-
-#endif /* __KERNEL__ */
-
 typedef unsigned long cycles_t;
 cycles_t get_cycles(void);
 
